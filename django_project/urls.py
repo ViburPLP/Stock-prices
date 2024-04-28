73ab059460773d9c1a542f0e8f15520c1ffe8dbb
ViburPLP <159889417+ViburPLP@users.noreply.github.com>
2024-04-28 14:23:49 +0000

    Hit rock bottom.

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import path
 from . import views
 from django.urls import path, include
 from stocksapp import views
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- stocksapp/views.py
+++ stocksapp/views.py
@@ -4,23 +4,25 @@ from django_project.models import StockPrice
 
 
 def dashboard(request):
-    api_key = "ZROA09JARY1NMF05"
-    symbol = "AAPL"
-    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
+  api_key = "ZROA09JARY1NMF05"
+  symbol = "AAPL"
+  url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
+  stock_prices = []
 
-    response = requests.get(url)
-    data = response.json()["Time Series (Daily)"]
+  response = requests.get(url)
+  data = response.json()
 
-    stock_prices = []
-    for date, values in data.items():
-        stock_price = StockPrice.objects.create(
-            date=date,
-            stock_symbol=symbol,
-            price=values["4. close"]
-        )
-        stock_prices.append(stock_price)
+  if "Time Series (Daily)" in data:
+            stock_prices = []
+            for date, values in data["Time Series (Daily)"].items():
+                stock_price = StockPrice.objects.create(
+                    date=date,
+                    stock_symbol=symbol,
+                    price=values["4. close"]
+            )
+                stock_prices.append(stock_price)
 
-    return render(request, 'stocks.html', {'stock_prices': stock_prices})
+  return render(request, 'stocks.html', {'stock_prices': stock_prices})
   
 def fetch_stock_prices(request):
     api_key = "ZROA09JARY1NMF05"  
